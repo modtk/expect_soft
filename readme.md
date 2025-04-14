5e975c2a6aff5906806400761d60ce4df538ee37
rosidae0 <82954131+rosidae@users.noreply.github.com>
2025-04-14 14:24:38 -0500

    Deprecation

--- readme.md
+++ readme.md
@@ -1,14 +1,14 @@
-# Archived as of 2025, use [set_hook](https://doc.rust-lang.org/std/panic/fn.set_hook.html) instead
+# expect_soft
 
-`.expect` but exit instead of panic
+> This library has been retired in favor of Rust's native [`set_hook`](https://doc.rust-lang.org/std/panic/fn.set_hook.html).
 
-# Example usage
+A small utility that was designed to provide a softer alternative to Rust's `.expect()` method. Instead of panicking, it prints an error message and exits the program. This was made before I knew about the `set_hook` built-in function.
 
 ```rust
 use expect_soft::ExpectSoft;
 use std::fs;
 
 fn main() {
-  fs::read_to_string("./readme.md").expect_soft("readme.md does not exist!");
+  fs::read_to_string("./readme.md").expect_soft("readme.md could not be found");
 }
 ```
--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,51 @@
+// This library is no longer maintained, supported, or updated.
+// See the README.md file for more information.
+
+#[deprecated(
+    since = "0.1.2",
+    note = "This crate is deprecated. See readme for more information"
+)]
 pub trait ExpectSoft<T, E> {
-  fn expect_soft(self, msg: &str) -> T;
-  fn expect_err_soft(self, msg: &str) -> E;
+    #[deprecated(
+        since = "0.1.2",
+        note = "This method is deprecated. See readme for more information"
+    )]
+    fn expect_soft(self, msg: &str) -> T;
+    #[deprecated(
+        since = "0.1.2",
+        note = "This method is deprecated. See readme for more information"
+    )]
+    fn expect_err_soft(self, msg: &str) -> E;
 }
 
 impl<T, E: std::fmt::Debug> ExpectSoft<T, E> for Result<T, E> {
-  fn expect_soft(self, msg: &str) -> T {
-    self.unwrap_or_else(|_| {
-      println!("FATAL: {}", msg);
-      std::process::exit(1);
-    })
-  }
-  
-  fn expect_err_soft(self, msg: &str) -> E {
-    self.err().unwrap_or_else(|| {
-      println!("FATAL: {}", msg);
-      std::process::exit(1);
-    })
-  }
+    fn expect_soft(self, msg: &str) -> T {
+        self.unwrap_or_else(|_| {
+            println!("FATAL: {}", msg);
+            std::process::exit(1);
+        })
+    }
+
+    fn expect_err_soft(self, msg: &str) -> E {
+        self.err().unwrap_or_else(|| {
+            println!("FATAL: {}", msg);
+            std::process::exit(1);
+        })
+    }
 }
 
 impl<T> ExpectSoft<T, ()> for Option<T> {
-  fn expect_soft(self, msg: &str) -> T {
-    self.unwrap_or_else(|| {
-      println!("FATAL: {}", msg);
-      std::process::exit(1);
-    })
-  }
-  
-  fn expect_err_soft(self, msg: &str) -> () {
-    if self.is_some() {
-      println!("FATAL: {}", msg);
-      std::process::exit(1);
+    fn expect_soft(self, msg: &str) -> T {
+        self.unwrap_or_else(|| {
+            println!("FATAL: {}", msg);
+            std::process::exit(1);
+        })
     }
-  }
+
+    fn expect_err_soft(self, msg: &str) {
+        if self.is_some() {
+            println!("FATAL: {}", msg);
+            std::process::exit(1);
+        }
+    }
 }
